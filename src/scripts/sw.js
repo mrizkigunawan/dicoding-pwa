66b5cd701812d2017c54bbf8d8cc9600e3e23040
Rizki Gunawan <rizkigunawan300@gmail.com>
2025-01-07 15:19:39 +0800

    Update: remove unused manual service worker file sw.js

--- src/scripts/sw.js
+++
@@ -1,9 +0,0 @@
-import { precacheAndRoute } from 'workbox-precaching';
-
-// do precaching
-precacheAndRoute(self.__WB_MANIFEST);
-
-self.addEventListener('install', () => {
-  console.log('Service Worker: installed');
-  self.skipWaiting();
-});
